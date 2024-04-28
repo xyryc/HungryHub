cb3e8689fc549cc029275c51bc0d88f68ed36fd9
Dusk <65917000+duskdev17@users.noreply.github.com>
2024-04-28 21:37:48 +0600

    Expo Router Screen

---
+++ src/app/(tabs)/menu/[id].tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack, useLocalSearchParams } from 'expo-router';
+
+const ProductDetailsScreen = () => {
+
+    const { id } = useLocalSearchParams();
+
+    return(
+        <View>
+            <Stack.Screen options={{title: 'Details: ' + id}}/>
+            <Text>ProductDetails for id: {id}</Text>
+        </View>
+    );
+};
+
+export default ProductDetailsScreen;
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,7 @@
 import Colors from '@/constants/Colors';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import { Product } from '../types';
+import { Link } from 'expo-router';
 
 export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
@@ -8,18 +9,22 @@ type ProductListItemProps = {
   product: Product,
 }
 
-const ProductListItem = ({product}: ProductListItemProps) =>{
+const ProductListItem = ({product}: ProductListItemProps) => {
 
   return (
-    <View style={styles.container}>
-      <Image 
-        source={{uri: product.image || defaultPizzaImage}} 
-        style={styles.image}
-        resizeMode='contain'
-      />
-     <Text style={styles.title}>{product.name}</Text>
-     <Text style={styles.price}>৳{product.price}</Text>
-    </View>
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image 
+          source={{uri: product.image || defaultPizzaImage}} 
+          style={styles.image}
+          resizeMode='contain'
+        />
+
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>৳{product.price}</Text>
+
+      </Pressable>
+    </Link>
   )
 }
 
