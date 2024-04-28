180a494d13d1d0b992b558b027fae00f769a54f3
Dusk <65917000+duskdev17@users.noreply.github.com>
2024-04-28 20:31:02 +0600

    FlatList implementation & UI fixes

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -12,7 +12,11 @@ const ProductListItem = ({product}: ProductListItemProps) =>{
 
   return (
     <View style={styles.container}>
-      <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image}/>
+      <Image 
+        source={{uri: product.image || defaultPizzaImage}} 
+        style={styles.image}
+        resizeMode='contain'
+      />
      <Text style={styles.title}>{product.name}</Text>
      <Text style={styles.price}>৳{product.price}</Text>
     </View>
@@ -26,12 +30,13 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%"
   },
   image: {
     width: '100%',
-    aspectRatio: 1
+    aspectRatio: 1,
   },
-
   title: {
     fontSize: 18,
     fontWeight: '600',
