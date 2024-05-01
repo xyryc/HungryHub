672b9c01a7c96c32fa52d8efd7c905974f4a3cbb
Dusk <65917000+duskdev17@users.noreply.github.com>
2024-05-01 23:39:27 +0600

    Update [id].tsx

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,16 +1,66 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
 
+  const product = products.find((p) => p.id.toString() == id);
+
+  if (!product) {
+    return <Text>Product Not Found</Text>;
+  }
+
   return (
-    <View>
-      <Stack.Screen options={{ title: "Details: " + id }} />
-      <Text>ProductDetails for id: {id}</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+
+      <Text>Select size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <View style={styles.size} key={size}>
+            <Text style={styles.sizeText}>{size}</Text>
+          </View>
+        ))}
+      </View>
+
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  size: {},
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+});
 
 export default ProductDetailsScreen;
