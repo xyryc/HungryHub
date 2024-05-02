390abc476ff6af1b9437c4828d80a0f63a9d629a
Dusk <65917000+duskdev17@users.noreply.github.com>
2024-05-02 13:21:53 +0600

    update Product Details Screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,15 +1,23 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import React from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
 import products from "@assets/data/products";
 import { defaultPizzaImage } from "@/components/ProductListItem";
+import { useState } from "react";
+import Button from "@components/Button";
 
 const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
 
+  const [selectedSize, setSelectedSize] = useState("M");
+
   const product = products.find((p) => p.id.toString() == id);
+
+  const addToCart = () => {
+    console.warn("Adding to cart, size: ", selectedSize);
+  };
 
   if (!product) {
     return <Text>Product Not Found</Text>;
@@ -27,13 +35,34 @@ const ProductDetailsScreen = () => {
       <Text>Select size</Text>
       <View style={styles.sizes}>
         {sizes.map((size) => (
-          <View style={styles.size} key={size}>
-            <Text style={styles.sizeText}>{size}</Text>
-          </View>
+          <Pressable
+            onPress={() => {
+              setSelectedSize(size);
+            }}
+            style={[
+              styles.size,
+              {
+                backgroundColor: selectedSize == size ? "gainsboro" : "white",
+              },
+            ]}
+            key={size}
+          >
+            <Text
+              style={[
+                styles.sizeText,
+                {
+                  color: selectedSize == size ? "black" : "gray",
+                },
+              ]}
+            >
+              {size}
+            </Text>
+          </Pressable>
         ))}
       </View>
 
       <Text style={styles.price}>${product.price}</Text>
+      <Button onPress={addToCart} text="Add to cart" />
     </View>
   );
 };
@@ -51,12 +80,21 @@ const styles = StyleSheet.create({
   price: {
     fontWeight: "bold",
     fontSize: 18,
+    marginTop: "auto",
   },
   sizes: {
     flexDirection: "row",
     justifyContent: "space-around",
+    marginVertical: 10,
   },
-  size: {},
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   sizeText: {
     fontSize: 20,
     fontWeight: "500",
