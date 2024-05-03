b2996bb436b2f15988174b286e27654d6ccc1f2a
Dusk <65917000+duskdev17@users.noreply.github.com>
2024-05-04 02:50:23 +0600

    Admin panel setup

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,71 @@
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import React from "react";
+import { Stack, router, useLocalSearchParams, useRouter } from "expo-router";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import { useState } from "react";
+import Button from "@components/Button";
+import { useCart } from "@/providers/CartProvider";
+import { PizzaSize } from "@/types";
+
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
+
+const ProductDetailsScreen = () => {
+  const { id } = useLocalSearchParams();
+  const { addItem } = useCart();
+
+  const router = useRouter();
+
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
+
+  const product = products.find((p) => p.id.toString() == id);
+
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+    addItem(product, selectedSize);
+    router.push("/cart");
+  };
+
+  if (!product) {
+    return <Text>Product Not Found</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  price: {
+    fontWeight: "500",
+    fontSize: 18,
+  },
+});
+
+export default ProductDetailsScreen;
