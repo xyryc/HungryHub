a4661a4c1e934535a4f3b9308850222a2d7e575a
Anik <65917000+duskdev17@users.noreply.github.com>
2024-05-21 09:47:20 +0600

    Auth UI
    - sign in UI
    - sign up UI
    - layout updated

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -9,8 +9,13 @@ const index = () => {
       <Link href={'/(user)'} asChild>
         <Button text="User" />
       </Link>
+
       <Link href={'/(admin)'} asChild>
         <Button text="Admin" />
+      </Link>
+
+      <Link href={'/sign-in'} asChild>
+        <Button text="Sign in" />
       </Link>
     </View>
   );
