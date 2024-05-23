8cf7093a23e7242de4f8dfccab089449ed9410df
Anik <65917000+duskdev17@users.noreply.github.com>
2024-05-24 01:21:44 +0600

    authentication using supabase
    - sign in and sign out authentication
    - admin & user panel authentication
    - auth guards to screens

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,22 +1,39 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import Button from '../components/Button';
-import { Link } from 'expo-router';
+import { View, Text } from "react-native";
+import React from "react";
+import Button from "../components/Button";
+import { Link, Redirect } from "expo-router";
+import { useAuth } from "@/providers/AuthProvider";
+import { ActivityIndicator } from "react-native";
+import { supabase } from "@/lib/supabase";
 
 const index = () => {
+  const { session, loading, isAdmin } = useAuth();
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  if (!session) {
+    return <Redirect href={"/sign-in"} />;
+  }
+
+  if (!isAdmin) {
+    return <Redirect href={"/(user)"} />;
+  }
+
+  // console.log(session);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-      <Link href={'/(user)'} asChild>
+    <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+      <Link href={"/(user)"} asChild>
         <Button text="User" />
       </Link>
 
-      <Link href={'/(admin)'} asChild>
+      <Link href={"/(admin)"} asChild>
         <Button text="Admin" />
       </Link>
 
-      <Link href={'/sign-in'} asChild>
-        <Button text="Sign in" />
-      </Link>
+      <Button onPress={() => supabase.auth.signOut()} text="Sign out" />
     </View>
   );
 };
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,19 +1,68 @@
 import { supabase } from "@/lib/supabase";
-import { PropsWithChildren, createContext, useEffect } from "react";
+import { Session } from "@supabase/supabase-js";
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
-type AuthData = {};
+type AuthData = {
+  session: Session | null;
+  profile: any;
+  loading: boolean;
+  isAdmin: boolean;
+};
 
-const AuthContext = createContext<AuthData>({});
+const AuthContext = createContext<AuthData>({
+  session: null,
+  loading: true,
+  profile: null,
+  isAdmin: false,
+});
 
 export default function AuthProvider({ children }: PropsWithChildren) {
+  const [session, setSession] = useState<Session | null>(null);
+  const [profile, setProfile] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchSession = async () => {
-      const result = await supabase.auth.getSession();
-      console.log(result);
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      setSession(session);
+
+      if (session) {
+        // fetch profile
+        const { data } = await supabase
+          .from("profiles")
+          .select("*")
+          .eq("id", session.user.id)
+          .single();
+        setProfile(data || null);
+      }
+
+      setLoading(false);
     };
 
     fetchSession();
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
   }, []);
 
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  // console.log(profile);
+
+  return (
+    <AuthContext.Provider
+      value={{ session, loading, profile, isAdmin: profile?.group === "ADMIN" }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
+
+export const useAuth = () => useContext(AuthContext);
